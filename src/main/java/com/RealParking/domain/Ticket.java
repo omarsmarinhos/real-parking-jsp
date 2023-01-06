172e7244e703481a7348677b5d486aa2af4b5d66
AntonioSotoAS <arturoantoniomontejosoto@gmail.com>
2023-01-05 19:45:32 -0500

    domain classes created

---
+++ src/main/java/com/RealParking/domain/Ticket.java
@@ -0,0 +1,75 @@
+package com.RealParking.domain;
+
+import jakarta.ejb.Local;
+
+import java.time.LocalDateTime;
+
+public class Ticket {
+
+    private int idTicket;
+    private String placa;
+    private LocalDateTime horaIngreso;
+
+    private LocalDateTime horaSalida;
+
+    private String state;
+
+    public Ticket(int idTicket, String placa, LocalDateTime horaIngreso, LocalDateTime horaSalida, String state) {
+        this.idTicket = idTicket;
+        this.placa = placa;
+        this.horaIngreso = horaIngreso;
+        this.horaSalida = horaSalida;
+        this.state = state;
+    }
+
+    public int getIdTicket() {
+        return idTicket;
+    }
+
+    public void setIdTicket(int idTicket) {
+        this.idTicket = idTicket;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public LocalDateTime getHoraIngreso() {
+        return horaIngreso;
+    }
+
+    public void setHoraIngreso(LocalDateTime horaIngreso) {
+        this.horaIngreso = horaIngreso;
+    }
+
+    public LocalDateTime getHoraSalida() {
+        return horaSalida;
+    }
+
+    public void setHoraSalida(LocalDateTime horaSalida) {
+        this.horaSalida = horaSalida;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    @Override
+    public String toString() {
+        return "Ticket{" +
+                "idTicket=" + idTicket +
+                ", placa='" + placa + '\'' +
+                ", horaIngreso=" + horaIngreso +
+                ", horaSalida=" + horaSalida +
+                ", state='" + state + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/RealParking/domain/User.java
@@ -0,0 +1,95 @@
+package com.RealParking.domain;
+
+import java.util.Arrays;
+
+public class User {
+    private int idUser;
+    private String username;
+    private String password;
+    private String fullName;
+
+    private String state;
+
+    private Role role;
+
+    private byte[] photo;
+
+    public User(int idUser, String username, String password, String fullName, String state, Role role, byte[] photo) {
+        this.idUser = idUser;
+        this.username = username;
+        this.password = password;
+        this.fullName = fullName;
+        this.state = state;
+        this.role = role;
+        this.photo = photo;
+    }
+
+    public int getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(int idUser) {
+        this.idUser = idUser;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public byte[] getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(byte[] photo) {
+        this.photo = photo;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "idUser=" + idUser +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", fullName='" + fullName + '\'' +
+                ", state='" + state + '\'' +
+                ", role=" + role +
+                ", photo=" + Arrays.toString(photo) +
+                '}';
+    }
+}
