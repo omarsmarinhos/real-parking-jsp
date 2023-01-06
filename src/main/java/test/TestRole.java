b7bd45229db2fc49c6a79680dcc03f2d4a38dd48
AntonioSotoAS <arturoantoniomontejosoto@gmail.com>
2023-01-06 15:34:02 -0500

    connection with jpa and hibernate configured and tested

---
+++ src/main/java/test/TestRole.java
@@ -0,0 +1,30 @@
+package test;
+
+import com.RealParking.persitence.entity.Role;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class TestRole {
+    private static EntityManagerFactory emf;
+    private static EntityManager manager;
+    public static void main(String[] args) {
+        emf = Persistence.createEntityManagerFactory("RealParkingPersistence");
+        manager = emf.createEntityManager();
+
+        List<Role> roles = (List<Role>) manager.createQuery("FROM Role",Role.class).getResultList();
+
+        for (Role role:
+             roles) {
+            System.out.println("idRol:"+role.getIdRol()+"\n");
+            System.out.println("Descripcion:"+role.getDescription()+"\n");
+            System.out.println("Estado:"+role.getState()+"\n");
+
+        }
+
+    }
+
+
+}
